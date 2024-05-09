119fa7d8f5dc8a8b76f3582ebf4b8b77dcddf39a
root <email>
2024-05-09 16:26:05 +0000

    quick checking

--- app.py
+++ app.py
@@ -1,86 +1,48 @@
 import streamlit as st
-import streamlit.components.v1 as components
-import json
+import pandas as pd
+from cachetools import cached
 
-PAGE_CONFIG = {"page_title":"More Examples","page_icon":":smiley:",
-               "layout":"centered"}
+from sqlalchemy import create_engine
+
+conn_string_imdb = 'mysql://{user}:{password}@{host}:{port}/{db}?charset=utf8'.format(
+    user='students',
+    password='0b+5bJWvXoA',
+    host = 'jsedocc7.scrc.nyu.edu',
+    port=3306,
+    db='imdb',
+    encoding = 'utf-8'
+)
+engine_imdb = create_engine(conn_string_imdb)
+
+PAGE_CONFIG = {"page_title":"More Examples","page_icon":":smiley:","layout":"centered"}
 st.set_page_config(**PAGE_CONFIG)
 
-
-
+def hist_all_movies(sql, engine):
+    df_movies = pd.read_sql_query(sql, con=engine)
+    fig = df_movies.rating.hist().get_figure()
+    st.pyplot(fig)
 
 def main():
-    st.title("Treemap with Highcharts in Streamlit")
+    st.title("Awesome Streamlit for ML")
+    st.subheader("How to run streamlit from colab")
+    menu = ["Page 1","Page 2", "Page 3"]
+    choice = st.sidebar.selectbox('Menu',menu)
+    if choice == 'Page 1':
+        st.subheader("Streamlit From Colab")
+        text = "this is some text that my code made"
+        st.write(text)
+    elif choice == 'Page 2':
+        SQL_script = st.text_area(label='SQL Input', value='select rating from movies LIMIT 100')
+        hist_all_movies(SQL_script, engine_imdb)
+    elif choice == 'Page 3':
+        slider_value = st.slider('Number of rows', min_value=3, max_value=1000)
 
-    data = [
-        {"id": "A", "name": "Technology", "color": "#434348", "value": 100, "change": 0.15},
-        {"id": "A1", "name": "Apple", "parent": "A", "value": 60, "change": 0.30},
-        {"id": "A2", "name": "Microsoft", "parent": "A", "value": 40, "change": -0.20},
-        {"id": "B", "name": "Financials", "color": "#90ed7d", "value": 200, "change": 0.50},
-        {"id": "B1", "name": "JPMorgan", "parent": "B", "value": 120, "change": 0.45},
-        {"id": "B2", "name": "Goldman Sachs", "parent": "B", "value": 80, "change": 0.55}
-    ]
-    html = f"""
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/treemap.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <div id="container" style="height: 400px; min-width: 310px; max-width: 800px; margin: 0 auto"></div>
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {{
-            var data = {json.dumps(data)};
-            Highcharts.chart('container', {{
-                series: [{{
-                    type: 'treemap',
-                    layoutAlgorithm: 'squarified',
-                    data: data,
-                    dataLabels: {{
-                        enabled: true,
-                        format: '{{point.name}}: {{point.change}}%'
-                    }},
-                    levels: [{{
-                        level: 1,
-                        dataLabels: {{
-                            enabled: true,
-                            align: 'left',
-                            verticalAlign: 'top',
-                            style: {{
-                                fontSize: '15px',
-                                fontWeight: 'bold'
-                            }}
-                        }}
-                    }}]
-                }}],
-                title: {{
-                    text: 'Sector Performance'
-                }},
-                tooltip: {{
-                    headerFormat: '',
-                    pointFormat: 'The change in {{point.name}} is <b>{{point.change}}%</b>'
-                }},
-                colorAxis: {{
-                    dataClasses: [{{
-                        from: -0.1,
-                        to: 0,
-                        color: '#ff3333',
-                        name: 'Negative'
-                    }}, {{
-                        from: 0.01,
-                        to: 0.2,
-                        color: '#48CFAD',
-                        name: 'Positive'
-                    }}]
-                }}
-            }});
-        }});
-    </script>
-    """
-    components.html(html,
-        height=600,
-    )
+        df_movies = pd.read_sql("select * from movies LIMIT " + str(slider_value), con=engine_imdb)
+        fig = df_movies.rating[~df_movies.rating.isna()].plot().get_figure()
+        st.pyplot(fig)
+        show_dataframe = st.checkbox('Display Dataframe')
+        if show_dataframe:
+            df_movies
 
-
-
-    # st.html(html)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
